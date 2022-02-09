5f12d243737e5275b52ec1c20f4eefb5500f5e2a
Petec0x0 <udehonyedikachi01@gmail.com>
2022-02-09 03:29:21 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { Lightning, Utils } from '@lightningjs/sdk'
 
 export default class App extends Lightning.Component {
-  static getFonts() {
-    return [{ family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') }]
-  }
 
   static _template() {
     return {
